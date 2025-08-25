b69b978ff56ec7c0d3c119d73cd65553658417d7
yesra29 <sarahfatima1924@gmail.com>
2025-08-25 22:18:50 +0530

    initial commit

---
+++ src/app/members/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import NavbarComponent from "@/components/navbar/navbar";
+import styles from "./members.css";
+
+export default function MembersPage() {
+         
+
+    return(
+        <NavbarComponent />
+        <button></button>
+    )
+}

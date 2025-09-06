91b0a34367df2850298b164a855266bfb35559b9
yesra29 <sarahfatima1924@gmail.com>
2025-09-07 00:35:57 +0530

    integration done for members page

--- src/lib/firebase/setup.js
+++ src/lib/firebase/setup.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need 
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; 
-
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,34 +10,33 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // console.log(firebaseConfig)
 
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-console.log(app, "firebase app instance");
-const db = getFirestore(app);
-console.log(db, "firestore instance");
+const app = initializeApp(firebaseConfig); //Firebase SDK function; initializes Firebase in project, firebaseConfig:contains the credentials (API key, project ID, etc.) for your specific Firebase project.
+console.log(app, "firebase app instance"); //prints the Firebase app instance to the console
+const db = getFirestore(app); //SDK function that initializes Firestore, db: database instance,use to perform queries like getDocs, setDoc, updateDoc
+console.log(db, "firestore instance"); //prints the Firestore instance object to the console
 
 let analytics = null;
 
 if (typeof window !== "undefined") {
-    isSupported().then((supported) => {
-        // console.log(supported, "is analytics supported");
-        if (supported) {
-            analytics = getAnalytics(app);
-            // console.log(analytics, "analytics instance")
-        }
-    });
+  isSupported().then((supported) => {
+    // console.log(supported, "is analytics supported");
+    if (supported) {
+      analytics = getAnalytics(app);
+      // console.log(analytics, "analytics instance")
+    }
+  });
 }
 
 const auth = getAuth(app);

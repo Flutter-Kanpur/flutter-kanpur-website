02b9682bb8f9aeb8052c6d415106ba30b1952c52
ROOKIEsarthak <officialsarthak22@gmail.com>
2025-11-03 20:09:20 +0530

    fixes

--- src/components/eventDetails/EventDetails.jsx
+++ src/components/eventDetails/EventDetails.jsx
@@ -3,22 +3,18 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-const EventDetails = ({short_description,description}) => {
-    const extraText = description.replace(short_description, "").trim();
+const EventDetails = ({ short_description, description }) => {
+  const extraText = description?.replace(short_description, "").trim();
+  return (
+    <Box >
+      <Typography variant="body1" sx={{ mb: 2, fontSize: '20px' }}>
+        {short_description}
+      </Typography>
 
-    return (
-
-        <Box >
-              <Typography variant="body1" sx={{ mb: 2,fontSize: '20px' }}>
-                {short_description}
-              </Typography>
-
-              <Typography variant="body1" sx={{ mb: 2 , fontSize: '20px' }}>
-                {extraText}
-              </Typography>
-              
-
-            </Box>
-    )
+      <Typography variant="body1" sx={{ mb: 2, fontSize: '20px' }}>
+        {extraText}
+      </Typography>
+    </Box>
+  )
 }
 export default EventDetails
